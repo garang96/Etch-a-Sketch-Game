b696a047dc0979af26053524f304b057bd9ef5b3
aliergarang <aliergarang16@gmail.com>
2022-02-18 12:30:15 +0300

    Add function to create grid cells

--- script.js
+++ script.js
@@ -4,7 +4,22 @@ const gridRange = document.querySelector('.grid-range');
 const generate = document.getElementById('generate');
 const clear = document.getElementById('clear');
 
-gridSize = gridRange.value;
+let gridSize = gridRange.value;
 console.log(gridSize);
 let cellWidth = gridSquares.offsetWidth / gridSize;
 console.log(cellWidth);
+
+
+const gridGame = (givenSize) => {
+    gridSquares.style.setProperty('--gridColumns', givenSize);
+    gridSquares.style.setProperty('--cellWidth', cellWidth);
+    for (let i = 1; i <= givenSize; i++) {
+        for (let j = 1; j <= givenSize; j++) {
+            const gridCell = document.createElement('div');
+            gridCell.classList.add('cell');
+            gridSquares.appendChild(gridCell);
+        }
+    }
+}
+
+gridGame(+gridSize);
